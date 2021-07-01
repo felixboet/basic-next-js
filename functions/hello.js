0e9ce64823c70e1566320476f302ea95ab9f0532
felixboet <mail@felixboettcher.de>
2021-07-01 21:36:11 +0200

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,10 @@
 exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || "World";
   console.log("process.env from function hello.js");
-  console.log(process.env);
+  console.log(process.env.NETLIFY);
+  console.log(process.env.BRANCH);
+  console.log(process.env.CONTEXT);
+  console.log(process.env.REPOSITORY_URL);
   return {
     statusCode: 200,
     body: `Hello ${subject}!`,
